5d264af0cf7382a9d3b99fbbe5d99e1910119126
veepee <66334129+vpontual@users.noreply.github.com>
2024-04-12 18:52:06 -0400

    updated server.js to show a login page

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const blogRoutes = require('./blogRoutes');
+// const blogRoutes = require('./blogRoutes');
 
 router.use('/users', userRoutes);
-router.use('/posts', blogRoutes);
+// router.use('/posts', blogRoutes);
 
 module.exports = router;
